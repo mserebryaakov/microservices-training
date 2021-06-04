6bd7f91bc5f1f74a0e768f80308ba96f9eaea5c6
mserebryaakov <maks.serebriakov2019@gmail.com>
2021-06-05 00:07:26 +0300

    lesson 3, add PUT

--- web/handler/products.go
+++ web/handler/products.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/mserebryaakov/microservices-training/data"
 )
@@ -24,6 +26,42 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		p.addProduct(rw, r)
 		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+
+		if len(r.URL.Path) != 2 {
+			p.l.Println("Invalid URL, len != 2")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URL more than one id")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URL more than one capture group")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URL unable to convert to number", idString)
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("got id", id)
+
+		p.updateProduct(id, rw, r)
 	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -50,4 +88,27 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
